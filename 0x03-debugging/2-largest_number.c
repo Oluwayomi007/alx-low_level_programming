fcf034c7144361389f511558f3088fbc5cf2906a
Oluwayomi007 <omoledare40@gmail.com>
2022-09-15 07:18:15 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,9 @@
 
 int largest_number(int a, int b, int c)
 {
+
 	int largest;
+
 	if (a >= b && a >= c)
 	{
 		largest = a;
@@ -23,5 +25,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
