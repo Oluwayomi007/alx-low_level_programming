43eb783d73d8396252c4482b59cf0627ecef628d
Oluwayomi007 <omoledar40@gmail.com>
2022-09-19 15:23:48 -0700

    task all

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
+#include "main.h"
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-
-#endif /*MAIN_H*/
+/**
+* reset_to_98 - takes an integer pointer and
+* updates the value it points to to 98
+* @n: an integer pointer that stores the address argument
+* Return: returns nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_rev - prints a given string of characters in reverse
+* @s: string parameter to be reversed
+* Return: Returns nothing
+*/
+void print_rev(char *s)
+{
+	int m, j;
+	int c = 0;
+
+	for (m = 0; 1 ; m++)
+	{
+		if (s[m] == 0)
+			break;
+		c++;
+	}
+	for (j = c - 1; j >= 0; j--)
+	{
+		printf("%c", s[j]);
+	}
+	printf("\n");
+}
